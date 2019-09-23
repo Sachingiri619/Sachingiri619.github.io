29b3e9d47666cf4ec857f3bb29ce5b7d52ecaf18
Sachingiri619 <sachingiri619@gmail.com>
2019-09-23 12:04:34 +0545

    lastest

---
+++ firebase-messaging-sw.js
@@ -0,0 +1,10 @@
+importScripts('https://www.gstatic.com/firebasejs/4.8.1/firebase-app.js');
+importScripts('https://www.gstatic.com/firebasejs/4.8.1/firebase-messaging.js');
+
+firebase.initializeApp({
+    messagingSenderId: '248890543145' // troque pelo seu sender id 
+});
+
+const messaging = firebase.messaging();
+
+
---
+++ precache-manifest.f60e362cbdc7d9987c97bb70372e8628.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "82a4338226506b385875",
+    "url": "/static/js/main.f428675b.chunk.js"
+  },
+  {
+    "revision": "2a6ae52ae36c07f8fed1",
+    "url": "/static/js/2.883cdf43.chunk.js"
+  },
+  {
+    "revision": "82a4338226506b385875",
+    "url": "/static/css/main.b70f8153.chunk.css"
+  },
+  {
+    "revision": "c57497bfee84e557c1dd44ab26d1167e",
+    "url": "/index.html"
+  }
+];
