707652505a39f1b37b61cecb00489840f44bfc98
Sachingiri619 <sachingiri619@gmail.com>
2019-10-21 15:14:08 +0545

    just now

---
+++ precache-manifest.19e2dab382f2e9225f61d2d675832911.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "acb00f6dc29fc4f66a3e",
+    "url": "/static/js/main.a07f3161.chunk.js"
+  },
+  {
+    "revision": "6a9fbfc9e464c7be94c3",
+    "url": "/static/js/2.8092d4f9.chunk.js"
+  },
+  {
+    "revision": "acb00f6dc29fc4f66a3e",
+    "url": "/static/css/main.fb1825a1.chunk.css"
+  },
+  {
+    "revision": "ef915935b4cc266bdee74b915c0d7480",
+    "url": "/index.html"
+  }
+];
